4e6c7633f32d3dc41e891c96b65112fac7748d63
Bisheng Liu <bisheng@postnl.nl>
2021-06-11 20:11:50 +0200

    add htrtp call to rsvp

---
+++ rsvp-loader/src/RSVPLoader/RSVPLoader/Program.cs
@@ -0,0 +1,48 @@
+﻿using MeetupGuilder.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+
+namespace RSVPLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prepare DI
+            var configuration = StandaloneConfigurationBuilder
+                .CreateConfigurationBuilder()
+                .Build();
+
+            string connectionString = "server=.;database=warehouse;User ID=app;password=app;";
+
+            var serviceProvider = new ServiceCollection()
+                .AddScoped<RSVPHelper>()
+                .AddDbContext<MeetupGuiderDbContext>(o =>
+                    o.UseLazyLoadingProxies()
+                    .UseSqlServer(connectionString))
+                .BuildServiceProvider();
+
+            // load services
+            using var scope = 
+                serviceProvider
+                .CreateScope();
+
+            using var repoContext = 
+                scope.ServiceProvider
+                .GetRequiredService<MeetupGuiderDbContext>();
+
+            var rsvpHelper = 
+                scope
+                .ServiceProvider
+                .GetService<RSVPHelper>();
+
+            //retrieve and save rsvps
+            _ = rsvpHelper.RetrieveRSVPs(repoContext);
+
+        }
+    }
+}
---
+++ rsvp-loader/src/RSVPLoader/RSVPLoader/RSVPHelper.cs
@@ -0,0 +1,50 @@
+﻿using MeetupGuilder.Entities;
+using RSVPLoader.MeetupGuider.Spark.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RSVPLoader
+{
+    public class RSVPHelper
+    {
+        private const string meetupUrl = "https://stream.meetup.com/2/rsvps";
+        private readonly JsonSerializerOptions _options;
+
+        public RSVPHelper()
+        {
+            _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true, IgnoreNullValues = true };
+            
+        }
+        public async Task RetrieveRSVPs(MeetupGuiderDbContext repoContext)
+        {
+            using HttpClient _client = new HttpClient();
+            _client.Timeout = TimeSpan.FromMilliseconds(Timeout.Infinite);
+            var request = new HttpRequestMessage(HttpMethod.Get, meetupUrl);
+            using var response = _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result;
+            response.EnsureSuccessStatusCode();
+
+            var stream = await response.Content.ReadAsStreamAsync();
+            using var reader = new StreamReader(stream);
+            while (!reader.EndOfStream)
+                try
+                {
+                    Console.WriteLine(reader.ReadLine());
+                    RSVP result = JsonSerializer.Deserialize<RSVP>(reader.ReadLine(), _options);
+                    var group = repoContext.RSVPGroups.FirstOrDefault();
+                    Console.WriteLine(group.City);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+        }
+    }
+}
