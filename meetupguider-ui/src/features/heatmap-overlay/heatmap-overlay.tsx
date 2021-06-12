e4528ea2961bb8f0d7fa349dc645bbd5ebe8599e
Bisheng Liu <bisheng@postnl.nl>
2021-06-12 18:56:24 +0200

    add more services for generating maps

--- meetupguider-ui/src/features/heatmap-overlay/heatmap-overlay.tsx
+++ meetupguider-ui/src/features/heatmap-overlay/heatmap-overlay.tsx
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import './heatmap-overlay.css';
+// @ts-ignore
+import Datamap from 'datamaps/dist/datamaps.world.hires.min.js';
 import AppState from '../../redux/app-state';
+import HeatmapService from './services/heatmap-service';
 import { getRSVPCitiesActionCreatorAsync, getCountryTopicsActionCreatorAsync } from './redux/actions/action-creators';
 
 const HeatmapOverlay = (): JSX.Element => {
@@ -8,16 +12,125 @@ const HeatmapOverlay = (): JSX.Element => {
   const dispatch = useDispatch();
   const heatmapState = useSelector((state: AppState) => state.heatmapState);
 
-  const loadRSVPCities = () => dispatch(getRSVPCitiesActionCreatorAsync());
+  const loadRSVPCities = () => {
+    dispatch(getRSVPCitiesActionCreatorAsync());
+    console.log(HeatmapService.getMapData(heatmapState.rsvpCities));
+    console.log(HeatmapService.getBubbleData(heatmapState.rsvpCities));
+    setIsLoaded(true);
+  };
+
   // const loadCountryTopics = () => dispatch(getCountryTopicsActionCreatorAsync('nl'));
   if (!isLoaded) {
     loadRSVPCities();
-    setIsLoaded(true);
-    console.log(heatmapState.rsvpCities);
   }
 
+  useEffect(() => {
+    loadRSVPCities();
+
+    const myMap = new Datamap({
+      element: document.getElementById('basic_choropleth'),
+      projection: 'mercator',
+      scope: 'world',
+      responsive: true,
+      fills: {
+        defaultFill: '#dddddd',
+      },
+      geographyConfig: {
+        popupOnHover: true,
+        highlightOnHover: true,
+        highlightFillColor: 'red',
+        popupTemplate(geography: any, data: any) {
+          return (
+            `<div class="hoverinfo">${
+              geography.properties.name
+            } - Year of formation:${
+              data.formationDate
+            } `
+          );
+        },
+      },
+      data: {
+        // Country Codes list https://countrycode.org/
+        USA: {
+          fillKey: 'firstWorld',
+          formationDate: 1900,
+        },
+        CHN: {
+          fillKey: 'thirdWorld',
+          formationDate: 1700,
+        },
+        JPN: {
+          fillKey: 'firstWorld',
+          formationDate: 1930,
+        },
+        AUS: {
+          fillKey: 'firstWorld',
+          formationDate: 1350,
+        },
+        IND: {
+          fillKey: 'thirdWorld',
+          formationDate: 1500,
+        },
+      },
+    });
+
+    // Manage responsiveness
+    window.addEventListener('resize', () => {
+      myMap.resize();
+    });
+
+    // // configure bubbles
+    // myMap.bubbles(
+    //   [
+    //     {
+    //       name: 'Tourist Spot 1',
+    //       radius: 5,
+    //       centered: 'IND',
+    //       tourist: 3800,
+    //       fillKey: 'boringSpot',
+    //     },
+    //     {
+    //       name: 'Tourist Spot 2',
+    //       radius: 5,
+    //       tourist: 1500,
+    //       centered: 'USA',
+    //       fillKey: 'boringSpot',
+    //     },
+    //     {
+    //       name: 'Tourist Spot 3',
+    //       radius: 5,
+    //       tourist: 15000,
+    //       fillKey: 'interestingSpot',
+    //       latitude: 11.415,
+    //       longitude: 165.1619,
+    //     },
+    //     {
+    //       name: 'Tourist Spot 4',
+    //       radius: 5,
+    //       tourist: 50000,
+    //       fillKey: 'interestingSpot',
+    //       latitude: 73.482,
+    //       longitude: 54.5854,
+    //     },
+    //   ],
+    //   {
+    //     popupTemplate(geo: any, data: any) {
+    //       return (
+    //         `<div class="hoverinfo">${
+    //           data.name
+    //         }<br /> No of Tourists:${
+    //           data.tourist}`
+    //       );
+    //     },
+    //   },
+    // );
+
+    // load legend
+    myMap.legend();
+  }, []);
+
   return (
-    <div>heapmap-overlay</div>
+    <div id="basic_choropleth" style={{ position: 'relative', width: 'auto%', height: '30%' }} className="heatmap_guider" />
   );
 };
 
--- meetupguider-ui/src/features/heatmap-overlay/types/index.ts
+++ meetupguider-ui/src/features/heatmap-overlay/types/index.ts
@@ -1,6 +1,10 @@
 import RSVPCity from './rsvp-city';
 import CountryTopic from './country-topic';
+import { MapData as MyMapData, MapDataCountry as MyMapDataCountry, CountryCode as MyCountryCode } from './map-data';
 
+export type MapData = MyMapData;
+export type MapDataCountry = MyMapDataCountry;
+export type CountryCode = MyCountryCode;
 export {
   RSVPCity,
   CountryTopic,
