0d771d307ee83825b4785456fc9c4cad1caa2424
Bisheng Liu <bisheng@postnl.nl>
2021-06-12 11:45:29 +0200

    add eslint

--- meetupguider-ui/src/App.tsx
+++ meetupguider-ui/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 const App = (): JSX.Element => (
-  <div className="App"></div>
+  <div className="App">meetup guider app</div>
 );
 
 export default App;
