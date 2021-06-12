7173a9fce76347c8b953a72224bb4eaf17838877
Bisheng Liu <bisheng@postnl.nl>
2021-06-12 09:12:33 +0200

    fix db connection

--- dockerfiles/db/docker-entrypoint.sh
+++ dockerfiles/db/docker-entrypoint.sh
@@ -7,11 +7,11 @@ if [ "$1" = '/opt/mssql/bin/sqlservr' ]; then
     function initialize_app_database() {
       # Wait a bit for SQL Server to start. SQL Server's process doesn't provide a clever way to check if it's up or not, and it needs to be up before we can import the application database
       if [ ! -f /tmp/app-initialized ]; then
-          sleep 15s
+          sleep 5s
           touch /tmp/app-initialized
       fi
 
-      sleep 30s;
+      sleep 10s;
       export PATH="$PATH:/opt/mssql-tools/bin"
 
       # Always run SQL files, be sure to add validations so it wont execute multiple times.
